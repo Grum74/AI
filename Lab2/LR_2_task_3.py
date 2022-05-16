64b684f4cec98e1237be13b8ed54512840264424
Anatoliy <askjs74@gmail.com>
2022-05-16 18:07:56 +0300

    add lab 2

---
+++ Lab2/LR_2_task_3.py
@@ -0,0 +1,108 @@
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+print("------------------------------------------------")
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width',
+'class']
+dataset = read_csv('url', names=names)
+# shape
+print("********shape********")
+print(dataset.shape)
+
+print("********Зріз даних********")
+print(dataset.head(20))
+
+# description
+print("********Опис даних********")
+print(dataset.describe())
+
+# classdistribution
+print("********Розподіл за класом********")
+print(dataset.groupby('class').size())
+
+# Діаграма розмаху
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+
+# Гістограма розподілу атрибутів датасета
+dataset.hist()
+pyplot.show()
+
+# Матриця діаграм розсіювання
+scatter_matrix(dataset)
+pyplot.show()
+
+# Розділення датасету
+array = dataset.values
+X = array[:, 0:4]
+y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+print("********Оцінка моделей********")
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Порівняння алгоритмів
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+print("********Оцінюємо прогноз********")
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+knn = KNeighborsClassifier(n_neighbors=1)
+
+knn.fit(X_train, Y_train)
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("Форма масиву X_new: {}".format(X_new.shape))
+prediction = knn.predict(X_new)
+print("Прогноз: {}".format(prediction))
+print("Спрогнозированная метка: {}".format(dataset['class']))
+
+
+y_pred = knn.predict(X_validation)
+print("Прогнози для тестового набору:\n {}".format(y_pred))
+
+print("Правильність тестового набору: {:.2f}".format(np.mean(y_pred == Y_validation)))
+
+print("Правильність тестового набору: {:.2f}".format(knn.score(X_validation, Y_validation)))
+
